1a757ceee2a0e368339a6c37d56c83867f250d33
tgmg <amorri40@gmail.com>
2007-04-12 15:01:54 +0000

    Fixed many things!
    git-svn-id: https://svn.code.sf.net/p/gjava/code@11 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ GMJ Project/src/org/gmj/gmjproject/toolbaractions/AddScript.java
@@ -0,0 +1,80 @@
+package org.gmj.gmjproject.toolbaractions;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.openide.util.Exceptions;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+
+public final class AddScript extends CallableSystemAction
+{
+    public static int scripts;
+    
+    public void performAction()
+    {
+        
+        
+        
+        
+        try     {
+            org.netbeans.api.project.Project pro = org.netbeans.api.project.ui.OpenProjects.getDefault().getMainProject();
+           //check if a project is open
+            if (pro == null)
+           {
+            return;   
+           }
+            java.io.File f = new java.io.File(pro.getProjectDirectory().getPath() +
+                                              "\\scripts\\NewScript" + scripts +
+                                              ".java");
+            
+
+            while (f.exists()) {
+                scripts++;
+                f = new java.io.File(pro.getProjectDirectory().getPath() +
+                                     "\\scripts\\NewScript" + scripts + ".java");
+            }
+            ;
+            
+            pro.getProjectDirectory().getFileObject("scripts").createData("NewScript" +
+                                                                         scripts +
+                                                                         ".java");
+            
+            //write to file
+            PrintWriter to = new PrintWriter(pro.getProjectDirectory().getFileObject("scripts").getFileObject("NewScript" +
+                                                                         scripts +
+                                                                         ".java").getOutputStream());
+            to.println("class "+"NewScript" + scripts +" {" );
+            to.println("    public static void performScript() {");
+            to.println("       //TODO add script code here");
+            to.println("    }");
+            to.println("}");
+            to.close();
+        }
+        catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+}
+    
+    public String getName()
+    {
+        return NbBundle.getMessage(AddScript.class, "CTL_AddScript");
+    }
+    
+    protected String iconResource()
+    {
+        return "org/gmj/gmjproject/toolbaractions/script.png";
+    }
+    
+    public HelpCtx getHelpCtx()
+    {
+        return HelpCtx.DEFAULT_HELP;
+    }
+    
+    protected boolean asynchronous()
+    {
+        return false;
+    }
+    
+}
