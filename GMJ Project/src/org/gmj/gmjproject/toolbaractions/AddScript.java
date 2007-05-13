230266252353f3e432fd7633858617543e68468d
tgmg <amorri40@gmail.com>
2007-05-13 21:04:08 +0000

    updated
    git-svn-id: https://svn.code.sf.net/p/gjava/code@75 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- GMJ Project/src/org/gmj/gmjproject/toolbaractions/AddScript.java
+++ GMJ Project/src/org/gmj/gmjproject/toolbaractions/AddScript.java
@@ -1,83 +1,189 @@
 package org.gmj.gmjproject.toolbaractions;
 
-import java.io.File;
+
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.io.PrintWriter;
 import org.openide.util.Exceptions;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataObject;
+import org.openide.nodes.Node;
+import org.openide.nodes.*;
+import org.openide.util.datatransfer.NewType;
+import org.openide.util.datatransfer.PasteType;
 
-public final class AddScript extends CallableSystemAction
-{
+public final class AddScript extends CallableSystemAction {
     public static int scripts;
     
-    public void performAction()
-    {
+    public void performAction() {
         
         
         
         
         try     {
             org.netbeans.api.project.Project pro = org.netbeans.api.project.ui.OpenProjects.getDefault().getMainProject();
-           //check if a project is open
-            if (pro == null)
-           {
-            return;   
-           }
+            //check if a project is open
+            if (pro == null) {
+                return;
+            }
             java.io.File f = new java.io.File(pro.getProjectDirectory().getPath() +
-                                              "\\scripts\\NewScript" + scripts +
-                                              ".java");
+                    "\\org\\gjava\\runner\\scripts\\NewScript" + scripts +
+                    ".java");
             
-
+            
             while (f.exists()) {
                 scripts++;
                 f = new java.io.File(pro.getProjectDirectory().getPath() +
-                                     "\\scripts\\NewScript" + scripts + ".java");
+                        "\\org\\gjava\\runner\\scripts\\NewScript" + scripts + ".java");
             }
             ;
             
-            pro.getProjectDirectory().getFileObject("scripts").createData("NewScript" +
-                                                                         scripts +
-                                                                         ".java");
-            
+            pro.getProjectDirectory().getFileObject("org").getFileObject("gjava").getFileObject("runner").getFileObject("scripts").createData("NewScript" +
+                    scripts +
+                    ".java");
             //write to file
-            PrintWriter to = new PrintWriter(pro.getProjectDirectory().getFileObject("scripts").getFileObject("NewScript" +
-                                                                         scripts +
-                                                                         ".java").getOutputStream());
-            to.println("package Scripts;" );
-            to.println("import org.gjava.runner;" );
+            PrintWriter to = new PrintWriter(pro.getProjectDirectory().getFileObject("org").getFileObject("gjava").getFileObject("runner").getFileObject("scripts").getFileObject("NewScript" +
+                    scripts +
+                    ".java").getOutputStream());
+            to.println("package org.gjava.runner.scripts;" );
+            to.println("import org.gjava.runner.*;" );
             to.println("class "+"NewScript" + scripts +" {" );
-            to.println(" // To use gml use gml.<functionname>" );
-            to.println(" Functions gml = new Functions();" );
+            to.println(" // To use gml/gjl use g.<functionname>" );
+            to.println(" Functions g = new Functions();" );
             to.println("    public static void performScript() {");
             to.println("       //TODO add script code here");
             to.println("    }");
             to.println("}");
             to.close();
-        }
-        catch (IOException ex) {
+            //            pro.getProjectDirectory().getFileObject("org").getFileObject("gjava").getFileObject("runner").getFileObject("scripts").getFileObject("NewScript" +
+            //                                                                         scripts +
+            //                                                                         ".java")
+            
+            DataFolder theDataObject =
+                    DataFolder.findFolder(pro.getProjectDirectory().getFileObject("org").getFileObject("gjava").getFileObject("runner").getFileObject("scripts"));
+            
+            
+            
+            //Get its default node - we'll wrap our node around it to change the
+            //display name, icon, etc.
+            Node realScriptsFolderNode = theDataObject.getNodeDelegate();
+            //realScriptsFolderNode.getPreferredAction().actionPerformed(new ActionEvent(this,0,""));
+            //DataObject datao = (DataObject)realScriptsFolderNode.getCookie(DataObject.class);
+           // datao.getFolder()
+//            realScriptsFolderNode = new Node(realScriptsFolderNode.getChildren()){
+//
+//                
+//public Node.Cookie getCookie(Class clazz) {
+//        if (clazz.isAssignableFrom(DataFolder.Index.class)) {
+//            DataFolder folder = (DataFolder)super.getCookie(DataFolder.class);
+//            if (folder != null) {
+//                return new DataFolder.Index(folder, this);
+//            }
+//        }
+//        return super.getCookie(clazz);
+//    }
+//        
+//                public Node cloneNode() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public Image getIcon(int arg0) {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public Image getOpenedIcon(int arg0) {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public HelpCtx getHelpCtx() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public boolean canRename() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public boolean canDestroy() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public PropertySet[] getPropertySets() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public Transferable clipboardCopy() throws IOException {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public Transferable clipboardCut() throws IOException {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public Transferable drag() throws IOException {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public boolean canCopy() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public boolean canCut() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public PasteType[] getPasteTypes(Transferable arg0) {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public PasteType getDropType(Transferable arg0, int arg1, int arg2) {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public NewType[] getNewTypes() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public boolean hasCustomizer() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public Component getCustomizer() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//
+//                public Handle getHandle() {
+//                    throw new UnsupportedOperationException("Not supported yet.");
+//                }
+//};
+            NodeOperation.getDefault().explore(realScriptsFolderNode);
+            
+        } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
-}
+    }
+
+    public AddScript() {
+    }
     
-    public String getName()
-    {
+    public String getName() {
         return NbBundle.getMessage(AddScript.class, "CTL_AddScript");
     }
     
-    protected String iconResource()
-    {
+    protected String iconResource() {
         return "org/gmj/gmjproject/toolbaractions/script.png";
     }
     
-    public HelpCtx getHelpCtx()
-    {
+    public HelpCtx getHelpCtx() {
         return HelpCtx.DEFAULT_HELP;
     }
     
-    protected boolean asynchronous()
-    {
+    protected boolean asynchronous() {
         return false;
     }
     
