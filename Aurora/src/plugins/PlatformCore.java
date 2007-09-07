ee722a4c4d02b44d1bab9688c50428cf249a9b20
tgmg <amorri40@gmail.com>
2007-09-07 19:14:17 +0000

    added platform core
    git-svn-id: https://svn.code.sf.net/p/gjava/code@245 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ Aurora/src/plugins/PlatformCore.java
@@ -0,0 +1,49 @@
+/*
+ * PlatformCore.java
+ * 
+ * Created on 07-Sep-2007, 19:34:25
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package plugins;
+
+/**
+ *
+ * @author Ali1
+ */
+public class PlatformCore extends PluginCore {
+    
+    public static void parseSprites()
+    {
+        //get list of sprites TODO
+        
+        // for each sprite parse and write into platform's language
+        
+    }
+
+    public static void parseActors()
+    {
+        //get list of actors TODO
+        
+        // for each actor parse and write into platform's language
+        
+    }
+    
+    public static void parseScenes()
+    {
+        //get list of scenes TODO
+        
+        // for each scene parse and write into platform's language
+        
+    }
+    
+    public static void parseClass()
+    {
+        //get list of class's TODO
+        
+        // for each class parse and write into platform's language
+        
+    }
+}
