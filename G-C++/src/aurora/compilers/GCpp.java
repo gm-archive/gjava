8f3260064c46232820489c06dba4a80c8fa464aa
luiscubal <luiscubal@gmail.com>
2007-09-10 20:38:19 +0000

    Created class. This is now a valid plugin.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@269 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ G-C++/src/aurora/compilers/GCpp.java
@@ -0,0 +1,28 @@
+/*
+ * GCpp.java
+ * 
+ * Created on 10/Set/2007, 21:31:59
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aurora.compilers;
+
+import plugins.*;
+/**
+ *
+ * @author Luís
+ */
+public class GCpp extends PluginCore{
+    public GCpp(){} //install
+    
+    //Since onMainWindowStart is called before
+    //the constructor, we need something after
+    //it.
+    public void onSplashDispose(){
+        init();
+    }
+    
+    public void init(){}
+}
