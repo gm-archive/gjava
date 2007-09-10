f004ec892a75c7fa2513a3d73a1d00bdcb601617
tgmg <amorri40@gmail.com>
2007-09-10 19:00:51 +0000

    Added G-Java
    git-svn-id: https://svn.code.sf.net/p/gjava/code@266 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ G-Java/src/gjava/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Main.java
+ * 
+ * Created on 10-Sep-2007, 19:59:58
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gjava;
+
+/**
+ *
+ * @author Ali1
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
