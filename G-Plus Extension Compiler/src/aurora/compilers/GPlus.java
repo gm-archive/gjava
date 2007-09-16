9bc7e256c7f4c30a24808bf37f20ecd2b12bf8fc
luiscubal <luiscubal@gmail.com>
2007-09-16 14:06:52 +0000

    Fixed a G-Plus bug. Added initial compilation code
    git-svn-id: https://svn.code.sf.net/p/gjava/code@336 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- G-Plus Extension Compiler/src/aurora/compilers/GPlus.java
+++ G-Plus Extension Compiler/src/aurora/compilers/GPlus.java
@@ -12,9 +12,14 @@ package aurora.compilers;
 import plugins.*;
 import managers.*;
 import core.*;
+import fileclass.*;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.*;
+import java.util.zip.ZipOutputStream;
+import java.util.zip.Deflater;
+import java.util.zip.ZipEntry;
 
 /**
  *
@@ -23,13 +28,41 @@ import java.awt.event.*;
 public class GPlus extends PluginCore{
     public GPlus(){}
     
-    public void compile(){}
+    public void compile(){
+        Project x = PluginHelper.getWindow().getCurrentProject();
+        PluginHelper.println(x.name);
+        if(x instanceof ExtensionProject){
+            try {
+                ZipOutputStream out = new ZipOutputStream(new FileOutputStream(x.name + ".ext"));
+                out.setLevel(Deflater.BEST_COMPRESSION);
+                out.putNextEntry(new ZipEntry("src/Java/"));
+                out.closeEntry();
+                out.putNextEntry(new ZipEntry("src/Cpp/"));
+                out.closeEntry();
+                out.putNextEntry(new ZipEntry("src/EGML/"));
+                out.closeEntry();
+                out.putNextEntry(new ZipEntry("ref/Java/"));
+                out.closeEntry();
+                out.putNextEntry(new ZipEntry("ref/Cpp/"));
+                out.closeEntry();
+                out.close();
+            }
+            catch(Exception e){
+                PluginHelper.println("Exception thrown: " + e.toString());
+            }
+        }
+        else{
+            PluginHelper.println("Project must be extension");
+        }
+    }
     
     @Override
     public void onSplashDispose(){
         JMenuItem compileext = new JMenuItem(PluginHelper.getLanguageSpecialMessage("GPlus.CompileCur"));
         
         compileext.addActionListener(new ActionListener(){
+            
+            @Override
             public void actionPerformed(ActionEvent evt){
                 compile();
             }
@@ -37,9 +70,9 @@ public class GPlus extends PluginCore{
         
         PluginHelper.println("!!!");
         
-        compileext = new JMenuItem(PluginHelper.getLanguageSpecialMessage("GPlus.CompileCur"));
         aurora.window.menus[3].add(compileext);
         
         VarsRegistry.setVariable("GPlus.MenuItem", compileext);
     }
 }
+
