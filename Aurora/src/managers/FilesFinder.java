75040f38e2c79451a46d3c0d0304695ea6943ea3
luiscubal <luiscubal@gmail.com>
2007-09-17 10:09:44 +0000

    Done FilesFinder - the class to find all the files of a specific type in a project.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@341 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ Aurora/src/managers/FilesFinder.java
@@ -0,0 +1,65 @@
+/*
+ * FilesFinder.java
+ * 
+ * Created on 17/Set/2007, 10:43:29
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package managers;
+
+import fileclass.*;
+
+/**
+ *
+ * @author Luís
+ */
+public class FilesFinder {
+    public static int getFileCountAt(Folder folder, String format){
+        if(folder==null||format==null)
+            return 0;
+        fileclass.Object o;
+        int a = folder.getChildNum();
+        for(int i = 0; i < folder.getChildNum(); i++){
+            o = folder.childAt(i);
+            if(o instanceof fileclass.File){
+                fileclass.File f = (fileclass.File) o;
+                if(!f.type.equals(format))
+                    a--;
+            }
+            if(o instanceof fileclass.Folder){
+                Folder f = (Folder) o;
+                int g = f.getChildNum();
+                g -= getFileCountAt(f, format);
+                a -= g;
+            }
+        }
+        return a;
+    }
+    
+    protected static int pos;
+    
+    public static fileclass.File[] getFilesAt(Folder folder, String format){
+        fileclass.File[] files = new fileclass.File[getFileCountAt(folder,format)];
+        pos = 0;
+        return _getFilesAt(files, folder, format);
+    }
+    
+    private static fileclass.File[] _getFilesAt(fileclass.File[] files, Folder folder, String format){
+        for(int i = 0; i < folder.getChildNum(); i++){
+            fileclass.Object o = folder.childAt(i);
+            if(o instanceof fileclass.File){
+                fileclass.File f = (fileclass.File) o;
+                if(f.type.equals(format)){
+                    files[pos++] = f;
+                }
+            }
+            if(o instanceof fileclass.Folder){
+                Folder f = (Folder) o;
+                files = _getFilesAt(files, f, format);
+            }
+        }
+        return files;
+    }
+}
