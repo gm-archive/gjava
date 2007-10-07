1664d12c2977af882ecfe010c81633e39d533abf
luiscubal <luiscubal@gmail.com>
2007-10-07 11:30:12 +0000

    Some improvements in the extension compiler G-Plus.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@384 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- G-Plus Extension Compiler/src/aurora/compilers/GPlus.java
+++ G-Plus Extension Compiler/src/aurora/compilers/GPlus.java
@@ -1,8 +1,8 @@
 /*
  * GPlus.java
- * 
+ *
  * Created on 14/Set/2007, 17:01:49
- * 
+ *
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -13,66 +13,103 @@ import plugins.*;
 import managers.*;
 import core.*;
 import fileclass.*;
-
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.zip.ZipOutputStream;
 import java.util.zip.Deflater;
 import java.util.zip.ZipEntry;
+import javax.crypto.*;
 
 /**
  *
  * @author Luís
  */
-public class GPlus extends PluginCore{
-    public GPlus(){}
-    
-    public void compile(){
-        Project x = PluginHelper.getWindow().getCurrentProject();
-        PluginHelper.println(x.name);
-        if(x instanceof ExtensionProject){
-            try {
-                ZipOutputStream out = new ZipOutputStream(new FileOutputStream(x.name + ".ext"));
-                out.setLevel(Deflater.BEST_COMPRESSION);
-                out.putNextEntry(new ZipEntry("src/Java/"));
-                out.closeEntry();
-                out.putNextEntry(new ZipEntry("src/Cpp/"));
-                out.closeEntry();
-                out.putNextEntry(new ZipEntry("src/EGML/"));
-                out.closeEntry();
-                out.putNextEntry(new ZipEntry("ref/Java/"));
-                out.closeEntry();
-                out.putNextEntry(new ZipEntry("ref/Cpp/"));
-                out.closeEntry();
-                out.close();
-            }
-            catch(Exception e){
-                PluginHelper.println("Exception thrown: " + e.toString());
-            }
+public class GPlus extends PluginCore {
+
+    public GPlus() {
+    }
+
+    public void dealWithJava(ExtensionProject p, ZipOutputStream z) {
+        fileclass.File[] files = p.getJavaFiles();
+        String crypted;
+        String dirs;
+        if (files == null) {
+            return;
         }
-        else{
-            PluginHelper.println("Project must be extension");
+        for (int i = 0; i < files.length; i++) {
+            if (files[i] != null) {
+                crypted = PluginHelper.encrypt((String) files[i].value);
+                fileclass.Folder x = files[i].root;
+                dirs = "src/Java/";
+                while (x instanceof Group && !(x instanceof JavaGroup)) {
+                    dirs += x.name;
+                    dirs += "/";
+                    x = ((Group) x).root;
+                }
+                try {
+                    z.putNextEntry(new ZipEntry(dirs + files[i].name + files[i].type));
+                    z.write(crypted.getBytes());
+                    z.closeEntry();
+                } catch (Exception e) {
+                }
+            }
         }
     }
-    
+
+    public void compile() {
+        Project x = PluginHelper.getWindow().getCurrentProject();
+        if (x != null) {
+            PluginHelper.println(x.name);
+            if (x instanceof ExtensionProject) {
+                ExtensionProject y = (ExtensionProject) x;
+                try {
+                    PluginHelper.println("Starting");
+                    ZipOutputStream out = new ZipOutputStream(new FileOutputStream(x.name + ".ext"));
+                    PluginHelper.println("Setting level");
+                    out.setLevel(Deflater.BEST_COMPRESSION);
+                    PluginHelper.println("Beginning compression");
+                    out.putNextEntry(new ZipEntry("src/Java/"));
+                    PluginHelper.println("Beginning Java");
+                    //out.closeEntry();
+                    dealWithJava(y, out);
+                    PluginHelper.println("Finished Java");
+                    out.putNextEntry(new ZipEntry("src/Cpp/"));
+                    //out.closeEntry();
+                    out.putNextEntry(new ZipEntry("src/EGML/"));
+                    //out.closeEntry();
+                    out.putNextEntry(new ZipEntry("ref/Java/"));
+                    //out.closeEntry();
+                    out.putNextEntry(new ZipEntry("ref/Cpp/"));
+                    //out.closeEntry();
+                    out.close();
+                } catch (Exception e) {
+                    PluginHelper.println("Exception thrown: " + e.toString() + "::" + e.getMessage());
+                }
+            } else {
+                PluginHelper.println("Project must be extension");
+            }
+        } else {
+            PluginHelper.println("No selected project");
+        }
+    }
+
     @Override
-    public void onSplashDispose(){
+    public void onSplashDispose() {
         JMenuItem compileext = new JMenuItem(PluginHelper.getLanguageSpecialMessage("GPlus.CompileCur"));
-        
-        compileext.addActionListener(new ActionListener(){
-            
+
+        compileext.addActionListener(new ActionListener() {
+
             @Override
-            public void actionPerformed(ActionEvent evt){
+            public void actionPerformed(ActionEvent evt) {
                 compile();
             }
         });
-        
+
         PluginHelper.println("!!!");
-        
+
         aurora.window.menus[3].add(compileext);
-        
+
         VarsRegistry.setVariable("GPlus.MenuItem", compileext);
     }
 }
-
