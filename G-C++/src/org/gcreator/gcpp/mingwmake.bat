f94a9aaa4e427022021e93f3b1d42b22db4b4df7
luiscubal <luiscubal@gmail.com>
2007-11-01 16:04:33 +0000

    Overall improvements in G-C++.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@496 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ G-C++/src/org/gcreator/gcpp/mingwmake.bat
@@ -0,0 +1,2 @@
+g++ ./Components/Actor.cpp ./Components/Image.cpp ./Components/Scene.cpp ./Components/Sprite.cpp ./Components/Tileset.cpp ./Components/Application.cpp ./Components/View.cpp ./Interaction/Keyboard.cpp ./Interaction/Mouse.cpp ./Scripting/GM4.cpp ./Scripting/string.cpp ./compilers/main.cpp -o ./Output/Windows.exe -lalleg -mwindows
+PAUSE
