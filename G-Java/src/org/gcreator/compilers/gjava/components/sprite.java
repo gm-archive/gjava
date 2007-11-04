705f6d4d1fa8d1aac7ed8ab65c56382e5eb3f80a
tgmg <amorri40@gmail.com>
2007-11-04 13:57:23 +0000

    added sprite
    git-svn-id: https://svn.code.sf.net/p/gjava/code@517 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ G-Java/src/org/gcreator/compilers/gjava/components/sprite.java
@@ -0,0 +1,18 @@
+/*
+ * sprite.java
+ * 
+ * Created on 04-Nov-2007, 13:56:17
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.gcreator.compilers.gjava.components;
+
+/**
+ *
+ * @author Ali
+ */
+public class sprite {
+
+}
