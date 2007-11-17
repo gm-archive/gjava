9eac70d189bb1654801bebfc55e4dbb5c7fc7f3e
luiscubal <luiscubal@gmail.com>
2007-11-17 16:04:07 +0000

    Added PluginActionPattern, which attempts to aid plugin actions.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@596 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ Aurora/src/org/gcreator/plugins/PluginActionPattern.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.gcreator.plugins;
+
+import org.gcreator.actions.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Luís
+ */
+public class PluginActionPattern extends ActionPattern{
+    public ImageIcon img;
+    
+    public String generateEGML(JComponent comp){
+        return "";
+    }
+    
+    public ImageIcon getStandardImage(){
+        return img;
+    }
+    
+    public void setStandardImage(ImageIcon img){
+        this.img = img;
+    }
+}
