c9a36ceaec7418c1583322ce801663d34ed3c76f
luiscubal <luiscubal@gmail.com>
2007-11-26 22:31:41 +0000

    Greatly changed G-C# system (part II)
    git-svn-id: https://svn.code.sf.net/p/gjava/code@683 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ G-C#/src/org/gcreator/compilers/GCS/libGCS/Components.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using org.gcreator.Support;
+using SdlDotNet.Graphics;
+
+namespace org.gcreator.Components
+{
+
+    public class Image
+    {
+        internal Surface texture;
+
+		public Image()
+		{
+			texture = null;
+		}
+		
+        public Image(string path)
+        {
+            texture = new Surface(path);
+        }
+    }
+
+    public class Sprite
+    {
+        private ArrayList images = new ArrayList();
+        private int originX, originY;
+        private Rectangle bounds;
+
+        public int getOriginX()
+        {
+            return originX;
+        }
+
+        public int getOriginY()
+        {
+            return originY;
+        }
+
+        public void setOriginX(int x)
+        {
+            originX = x;
+        }
+
+        public void setOriginY(int y)
+        {
+            originY = y;
+        }
+
+        public void setOrigin(int x, int y)
+        {
+            originX = x;
+            originY = y;
+        }
+
+        public Image getImage(int pos)
+        {
+            return (Image) images[pos];
+        }
+
+        public int getImageCount()
+        {
+            return images.ToArray().Length;
+        }
+
+        public Image[] getImageArray()
+        {
+            return (Image[])images.ToArray(typeof(Image));
+        }
+
+        public void setImageArray(Image[] images)
+        {
+            this.images = new ArrayList();
+            this.images.AddRange(images);
+        }
+
+        public int getWidth()
+        {
+            return 0;
+        }
+
+        public int getHeight()
+        {
+            return 0;
+        }
+
+        public Rectangle getBounds()
+        {
+            return bounds;
+        }
+
+        public void setBounds(Rectangle bounds)
+        {
+            this.bounds = bounds;
+        }
+    }
+
+    public class Sound
+    {
+
+    }
+
+    public class Actor
+    {
+        private int x, y, z, spritepos, spritemovdelay, spritet, spritett;
+        //spritet-current position(only on -1 mode)
+        //spritett-time to fill delay
+        private Sprite sprite = null;
+        private double gravity, gravitydir;
+        private bool visible, solid, persistent;
+
+        public int getSpritePosition()
+        {
+            return spritepos;
+        }
+
+        public int getSpriteMovingDelay()
+        {
+            return spritemovdelay;
+        }
+
+        public void setSpritePosition(int spritepos)
+        {
+            this.spritepos = spritepos;
+        }
+
+        public int getCurrentSpritePosition()
+        {
+            if (spritepos != -1)
+                return spritepos;
+            return spritet;
+        }
+
+        public void setSpriteMovingDelay(int delay)
+        {
+            spritemovdelay = delay;
+        }
+
+        public Sprite getSprite()
+        {
+            return sprite;
+        }
+
+        public void setSprite(Sprite sprite)
+        {
+            this.sprite = sprite;
+        }
+
+        public double getGravity()
+        {
+            return gravity;
+        }
+
+        public double getGravityDirection()
+        {
+            return gravitydir;
+        }
+
+        public void setGravity(double gravity)
+        {
+            this.gravity = gravity;
+        }
+
+        public void setGravityDirection(double direction)
+        {
+            gravitydir = direction;
+        }
+
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+
+        public void setDepth(int depth)
+        {
+            z = depth;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public int getDepth()
+        {
+            return z;
+        }
+
+        public bool isSolid()
+        {
+            return solid;
+        }
+
+        public void setSolid(bool solid)
+        {
+            this.solid = solid;
+        }
+
+        public bool isVisible()
+        {
+            return visible;
+        }
+
+        public void setVisible(bool visible)
+        {
+            this.visible = visible;
+        }
+		
+		public bool isPersistent()
+        {
+            return persistent;
+        }
+
+        public void setPersistent(bool persistent)
+        {
+            this.persistent = persistent;
+        }
+		
+		public virtual void BeginStep(){}
+		public virtual void Step(){}
+		public virtual void EndStep(){}
+    }
+
+    public class Scene
+    {
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
---
+++ G-C#/src/org/gcreator/compilers/GCS/libGCS/Native.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using org.gcreator.Components;
+using SdlDotNet.Core;
+using SdlDotNet.Graphics;
+
+namespace org.gcreator.Native
+{
+	public class CSharp
+	{
+		public static org.gcreator.Support.Color ToSupportColor(System.Drawing.Color c)
+		{
+			return new org.gcreator.Support.Color(c.R, c.G, c.B);
+		}
+		
+		public static System.Drawing.Color ToDrawingColor(org.gcreator.Support.Color c)
+		{
+			return System.Drawing.Color.FromArgb(c.getRed(), c.getGreen(), c.getBlue());
+		}
+		
+		public static System.Drawing.Rectangle ToDrawingRectangle(org.gcreator.Support.Rectangle r)
+		{
+			return new System.Drawing.Rectangle(r.x, r.y, r.width, r.height);
+		}
+	}
+	
+    public class SDL
+    {
+		public static void DrawToSurface(Image image, Surface screen, org.gcreator.Support.Rectangle source, org.gcreator.Support.Rectangle destination)
+		{
+			DrawToSurface(image.texture, screen, CSharp.ToDrawingRectangle(source), CSharp.ToDrawingRectangle(destination));
+		}
+		
+		public static void DrawToSurface(Surface image, Surface screen, org.gcreator.Support.Rectangle source, org.gcreator.Support.Rectangle destination)
+		{
+			DrawToSurface(image, screen, CSharp.ToDrawingRectangle(source), CSharp.ToDrawingRectangle(destination));
+		}
+		
+		public static void DrawToSurface(Image image, Surface screen, System.Drawing.Rectangle source, System.Drawing.Rectangle destination)
+		{
+			DrawToSurface(image.texture, screen, source, destination);
+		}
+		
+		public static void DrawToSurface(Surface image, Surface screen, System.Drawing.Rectangle source, System.Drawing.Rectangle destination)
+		{
+			Surface s = image.CreateSurfaceFromClipRectangle(source);
+			DrawToSurface(s, screen, destination);
+		}
+		
+		public static void DrawToSurface(Image image, Surface screen, org.gcreator.Support.Rectangle destination)
+		{
+			DrawToSurface(image.texture, screen, CSharp.ToDrawingRectangle(destination));
+		}
+		
+		public static void DrawToSurface(Image image, Surface screen, System.Drawing.Rectangle destination)
+		{
+			DrawToSurface(image.texture, screen, destination);
+		}
+		
+		public static void DrawToSurface(Surface image, Surface screen, org.gcreator.Support.Rectangle destination)
+		{
+			screen.Blit(image, CSharp.ToDrawingRectangle(destination));
+		}
+		
+		public static void DrawToSurface(Surface image, Surface screen, System.Drawing.Rectangle destination)
+		{
+			screen.Blit(image, destination);
+		}
+	
+		public Surface getSDLTexture(Image img)
+		{
+			return img.texture;
+		}
+		
+		public Image makeImage(Surface tex)
+		{
+			Image i = new Image();
+			i.texture = tex;
+			return i;
+		}
+	
+        public class Game
+        {
+            public Scene currentScene;
+            public Scene[] scenes;
+			public static Game game;
+
+			public Surface screen, master;
+			
+			private bool fullscreen, resizable;
+			private int width, height; //Initial
+			
+            public Game(Scene[] scenelist, bool fullscreen, bool resizable, int width, int height, string title)
+            {
+				game = this;
+				this.fullscreen = fullscreen;
+				this.resizable = resizable;
+				this.width = width;
+				this.height = height;
+                scenes = scenelist;
+                currentScene = scenelist[0];
+                screen = Video.SetVideoMode(width, height, resizable, false, fullscreen);
+				master = new Surface(width, height);
+                Video.WindowCaption = title;
+                Events.Quit += new EventHandler<QuitEventArgs>(this.Quit);
+				Events.VideoResize += new EventHandler<VideoResizeEventArgs>(this.Resize);
+				Events.Tick += new EventHandler<TickEventArgs>(this.Tick);
+            }
+			
+			private void Tick(object o, TickEventArgs t)
+			{
+				Draw();
+			}
+
+            private void Draw()
+            {
+                currentScene.Draw();
+				master.Update();
+				screen.Blit(master);
+				screen.Update();
+            }
+			
+			internal void UpdateVideoMode(int Width, int Height)
+			{
+				screen = Video.SetVideoMode(Width, Height, resizable, false, fullscreen);
+				master = new Surface(Width, Height);
+			}
+			
+			internal void UpdateVideoMode()
+			{
+				UpdateVideoMode(width, height);
+			}
+
+            private void Quit(object sender, QuitEventArgs e)
+            {
+                Events.QuitApplication();
+            }
+
+            public void Run()
+            {
+                Events.Run();
+            }
+			
+			private void Resize(object sender, VideoResizeEventArgs e)
+			{
+				UpdateVideoMode(e.Width, e.Height);
+			}
+        }
+    }
+}
