f1d2186b5c0d6ae2c759947bbc2bd31289f5a326
tgmg <amorri40@gmail.com>
2007-12-01 22:03:33 +0000

    Added 2 new projects GM importer and G-javaLibrary
    git-svn-id: https://svn.code.sf.net/p/gjava/code@696 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- Aurora/dodist/opencmd.bat
+++
@@ -1,3 +0,0 @@
-echo off
-echo This is just an utility to allow command line commands in Windows easily. Not needed in Linux KDE.
-cmd
--- G-C++/src/org/gcreator/gcpp/mingwmake.bat
+++
@@ -1,2 +0,0 @@
-g++ ./Components/Actor.cpp ./Components/Image.cpp ./Components/Scene.cpp ./Components/Sprite.cpp ./Components/Tileset.cpp ./Components/Application.cpp ./Components/View.cpp ./Interaction/Keyboard.cpp ./Interaction/Mouse.cpp ./Scripting/GM4.cpp ./Scripting/string.cpp ./compilers/main.cpp -o ./Output/Windows.exe -lalleg -mwindows
-PAUSE
