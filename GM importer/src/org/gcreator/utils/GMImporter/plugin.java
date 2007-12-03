03bc53aeeb5fb28ed45e1a6dcb4e3f85cbbd8e04
luiscubal <luiscubal@gmail.com>
2007-12-03 22:50:46 +0000

    Improved GM Importer.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@710 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- GM importer/src/org/gcreator/utils/GMImporter/plugin.java
+++ GM importer/src/org/gcreator/utils/GMImporter/plugin.java
@@ -5,6 +5,9 @@
 
 package org.gcreator.utils.GMImporter;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import javax.swing.*;
 import org.gcreator.plugins.*;
 
@@ -17,6 +20,45 @@ public class plugin extends PluginCore{
     public void onSplashDispose(){
         JMenuItem GM6 = new JMenuItem();
         VarsRegistry.setVariable("GMImporter.GM6", GM6);
+        GM6.setText("Game Maker 6 Project");
+        GM6.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt)
+            {
+                onGM6();
+            }
+        });
         PluginHelper.addMenuItem(11, GM6);
+    }
+    
+    private static FileDialog dialog = null;
+    public static final int MODE_GM6 = 1;
+    
+    public void onGM6()
+    {
+        if(dialog==null){
+            dialog = new FileDialog(PluginHelper.getWindow());
+        }
+        dialog.setFilenameFilter(new FilenameFilter(){
+            public boolean accept(File dir, String name){
+                 if(dir.isDirectory()||name.endsWith(".gm6"))
+                     return true;
+                 return false;
+            }
+        });
+        dialog.setVisible(true);
+        String fname = dialog.getFile();
+        if(fname==null)
+            return;
+        importFile(fname, MODE_GM6);
+    }
+    
+    public void importFile(String fname, int mode)
+    {
+        if(mode==MODE_GM6)
+        {
+            try{
+                GM6Importer imp = new GM6Importer(fname);
+            }catch(Exception e){}
+        }
     }
 }
