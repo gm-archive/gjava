677850498629c255d6dce11510dc6e4694f952d6
luiscubal <luiscubal@gmail.com>
2007-12-17 14:08:35 +0000

    Improved GM importer.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@731 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- GM importer/src/org/gcreator/utils/GMImporter/GM6Importer.java
+++ GM importer/src/org/gcreator/utils/GMImporter/GM6Importer.java
@@ -3,13 +3,14 @@
  *
  * Created on 6/Out/2007, 19:58:31
  *
- * Based on Lateral Game MakerGm6FileReader
+ * Based on Lateral Game Maker GmFileReader
  */
 
 package org.gcreator.utils.GMImporter;
 
 import java.io.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.zip.*;
 import javax.swing.*;
 import org.gcreator.core.*;
@@ -24,7 +25,11 @@ import org.gcreator.plugins.*;
  * @author Luís
  */
 public class GM6Importer {
-
+    public static final byte LOADBAR_NONE = 0;
+    public static final byte LOADBAR_DEFAULT = 1;
+    public static final byte LOADBAR_CUSTOM = 2;
+    
+    
     private GM6Importer() {
     }
 
@@ -64,7 +69,7 @@ public class GM6Importer {
             String msg = "Unsupported"; //$NON-NLS-1$
             throw new Gm6FormatException(String.format(msg,"",ver)); //$NON-NLS-1$
 	}
-        readSettings(settings, c);
+        SettingsValues values = readSettings(settings, c);
         
         in.close();
         
@@ -72,14 +77,15 @@ public class GM6Importer {
         PluginHelper.getWindow().workspace.updateUI();
     }
     
-    private void readSettings(org.gcreator.fileclass.File settings, Gm6FileContext c) throws IOException,Gm6FormatException,
+    private SettingsValues readSettings(org.gcreator.fileclass.File settings, Gm6FileContext c) throws IOException,Gm6FormatException,
 			DataFormatException
     {
         SettingsValues value;
-        TabValues Graphics, Resolution;
+        TabValues Graphics, Resolution, Other;
         settings.value = value = new SettingsValues();
         value.setVariable("Graphics", Graphics = new TabValues("Graphics"));
         value.setVariable("Resolution", Resolution = new TabValues("Resolution"));
+        value.setVariable("Other", Other = new TabValues("Other"));
        
         GmStreamDecoder in = c.in;
         in.read4(); //Game ID - unused
@@ -95,11 +101,55 @@ public class GM6Importer {
         boolean dontDrawBorder = in.readBool();
         boolean displayCursor = in.readBool();
         int scaling = in.read4();
+        if (ver == 530)
+            in.skip(8); //"fullscreen scale" & "only scale w/ hardware support" 
+        else{
         Graphics.setVariable("resize", in.readBool());
         boolean alwaysOnTop = alwaysOnTop = in.readBool();
         in.read4(); //Color outside room
+        }
         Resolution.setVariable("setres", in.readBool());
-        Resolution.setVariable("depth", in.read4() * 16);
-        Resolution.setVariable("resol", (Integer) in.read4());
+        if (ver == 530)
+			{
+			in.skip(8); //Color Depth, Exclusive Graphics
+			byte b = (byte) in.read4();
+			if (b == 6)
+				Resolution.setVariable("resol", (Integer) 0);
+			else if (b == 5)
+				Resolution.setVariable("resol", (Integer) 1);
+			else
+				Resolution.setVariable("resol", (Integer) (b-2));
+			b = (byte) in.read4();
+			Resolution.setVariable("frequency", (Integer) ((b == 4) ? 0 : (b + 1)));
+			in.skip(8); //vertical blank, caption in fullscreen
+			}
+        else{
+            Resolution.setVariable("depth", (Integer) (in.read4() * 16));
+            Resolution.setVariable("resol", (Integer) in.read4());
+            Resolution.setVariable("frequency", (Integer) in.read4());
+        }
+        boolean dontShowButtons = in.readBool();
+        boolean useSynchronization = false;
+	if (ver > 530) useSynchronization = in.readBool();
+        Other.setVariable("F4", (Boolean) in.readBool());
+        boolean F1 = in.readBool();
+        Other.setVariable("ESC", (Boolean) in.readBool());
+        boolean F5andF6 = in.readBool();
+        if (ver == 530) in.skip(8); //unknown bytes, both 0
+	if (ver == 702) in.skip(8); //F9 screenshot, Treat close as esc
+        int priority = in.read4();
+        boolean losefocusfreeze = in.readBool();
+        int loadBarMode = in.read4();
+        BufferedImage back = null, front = null;
+        if (loadBarMode == LOADBAR_CUSTOM)
+	{
+            if (in.read4() != -1) back = in.readImage(); //back
+            if (in.read4() != -1) front = in.readImage(); //front
+        }
+        boolean showCustomLoadImage = in.readBool();
+        BufferedImage loadingImage = null;
+        if (showCustomLoadImage) if (in.read4() != -1) loadingImage = in.readImage();
+        boolean imagePartiallyTransparent = in.readBool();
+        return value;
     }
 }
