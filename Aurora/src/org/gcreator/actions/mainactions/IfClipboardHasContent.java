f71eb33c730a39ecdf944364bf5b80470cd992e3
luiscubal <luiscubal@gmail.com>
2007-12-27 16:59:55 +0000

    Added clipboard category and new action.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@786 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ Aurora/src/org/gcreator/actions/mainactions/IfClipboardHasContent.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.gcreator.actions.mainactions;
+
+import org.gcreator.actions.*;
+import org.gcreator.actions.components.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author luis
+ */
+public class IfClipboardHasContent extends ActionPattern{
+    public static ImageIcon img = null;
+    public IfClipboardHasContent(){
+        super();
+    }
+    
+    public void setStandardImage(ImageIcon img){
+        IfClipboardHasContent.img = img;
+    }
+    
+    public ImageIcon getStandardImage(){
+        return img;
+    }
+    
+    public  JComponent createNewPanel(org.gcreator.actions.Action action){
+        YesOrNoIfPanel panel = new YesOrNoIfPanel();
+        return panel;
+    }
+    
+    public String getStandardText(JComponent panel){
+        if(panel==null||!(panel instanceof YesOrNoIfPanel)||!((YesOrNoIfPanel) panel).NotCheckbox.isSelected())
+            return "If Clipboard has Text";
+        return "If Clipboard is empty";
+    }
+    
+    public String generateGCL(JComponent panel){
+        if(panel==null||!(panel instanceof YesOrNoIfPanel)||!((YesOrNoIfPanel) panel).NotCheckbox.isSelected())
+            return "if(Clipboard.hasText())\n";
+        return "if(!Clipboard.hasText())\n";
+    }
+}
--- Aurora/src/org/gcreator/editors/ActorEditor.java
+++ Aurora/src/org/gcreator/editors/ActorEditor.java
@@ -49,8 +49,11 @@ public class ActorEditor extends TabPanel {
     
     DefaultComboBoxModel actmodel = new DefaultComboBoxModel();
     static {
-        
-        ActionCategory main = new ActionCategory(), move = new ActionCategory(), draw2D = new ActionCategory(), platform = new ActionCategory();
+        ActionCategory main = new ActionCategory();
+        ActionCategory move = new ActionCategory();
+        ActionCategory draw2D = new ActionCategory();
+        ActionCategory clipboard = new ActionCategory();
+        ActionCategory platform = new ActionCategory();
         main.add(new org.gcreator.actions.mainactions.StartOfABlock());
         main.add(new org.gcreator.actions.mainactions.EndOfABlock());
         main.add(new org.gcreator.actions.mainactions.Comment());
@@ -64,11 +67,15 @@ public class ActorEditor extends TabPanel {
         draw2D.add(new org.gcreator.actions.mainactions.AddImageToSprite());
         draw2D.name = "Draw 2D";
         draw2D.icon = new ImageIcon(ActorEditor.class.getResource("/org/gcreator/actions/images/Draw2D.png"));
+        clipboard.name = "Clipboard";
+        clipboard.icon = new ImageIcon(ActorEditor.class.getResource("/org/gcreator/resources/general/paste.png"));
+        clipboard.add(new org.gcreator.actions.mainactions.IfClipboardHasContent());
         platform.name = "Platform";
-       platform.add(new org.gcreator.actions.platform.CmdAction());
+        platform.add(new org.gcreator.actions.platform.CmdAction());
         actionCats.add(main);
         actionCats.add(move);
         actionCats.add(draw2D);
+        actionCats.add(clipboard);
         actionCats.add(platform);
         
     }
