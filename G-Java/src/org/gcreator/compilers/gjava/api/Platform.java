d24e283287bcc128c4b6945faf7265884f790e29
tgmg <amorri40@gmail.com>
2007-12-27 03:54:42 +0000

    Updated Platform
    git-svn-id: https://svn.code.sf.net/p/gjava/code@785 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- G-Java/src/org/gcreator/compilers/gjava/api/Platform.java
+++ G-Java/src/org/gcreator/compilers/gjava/api/Platform.java
@@ -1,57 +1,54 @@
 /*
- * platform.java
+ * Platform.java
  * 
- * Created on 11-Sep-2007, 04:13:49
  * 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
  */
 
 package org.gcreator.compilers.gjava.api;
 
 public class Platform {
 
-        public static String get_os_name() {
+        public static String getOsName() {
             return System.getProperty("os.name", "unknown");
         }
         
-        public static boolean is_windows(){
-            return get_os_name().toLowerCase().startsWith("Win");
+        public static boolean isWindows(){
+            return getOsName().toLowerCase().startsWith("Win");
         }
         
-        public static boolean is_linux(){
-            return get_os_name().toLowerCase().startsWith("Linux") ;
+        public static boolean isLinux(){
+            return getOsName().toLowerCase().startsWith("Linux") ;
         }
         
-        public static boolean is_mac(){
-            return get_os_name().toLowerCase().startsWith("Mac");
+        public static boolean isMac(){
+            return getOsName().toLowerCase().startsWith("Mac");
         }
         
-        public static boolean is_solaris(){
-            return get_os_name().toLowerCase().startsWith("SunOS");
+        public static boolean isSolaris(){
+            return getOsName().toLowerCase().startsWith("SunOS");
         }
         
-        public static boolean is_free_bsd(){
-            return get_os_name().toLowerCase().startsWith("FreeBSD");
+        public static boolean isFreeBSD(){
+            return getOsName().toLowerCase().startsWith("FreeBSD");
         }
         
-        public static boolean is_windows_xp(){
-            return is_windows() && get_os_name().toLowerCase().indexOf("xp") > -1;
+        public static boolean isWindowsXP(){
+            return isWindows() && getOsName().toLowerCase().indexOf("xp") > -1;
         }
         
-        public static boolean is_unknown_os(){
-            return get_os_name().equals("unknown");
+        public static boolean isUnknown(){
+            return getOsName().equals("unknown");
         }
         
-        public static boolean is_windows_nt(){
-            return is_windows() && get_os_name().toLowerCase().indexOf("nt") > -1;
+        public static boolean isWindowsNT(){
+            return isWindows() && getOsName().toLowerCase().indexOf("nt") > -1;
         }
         
-        public static boolean is_windows_vista(){
-            return is_windows() && get_os_name().toLowerCase().indexOf("vista") > -1;
+        public static boolean isWindowsVista(){
+            return isWindows() && getOsName().toLowerCase().indexOf("vista") > -1;
         }
         
-        public static String get_os_version(){
+        public static String getOSVersion(){
             return System.getProperty("os.version", "-1");
         }
         
