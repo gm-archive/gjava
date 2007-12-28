efa161cb9e64b5e4e08c12ad7b1a19db1be4b62c
tgmg <amorri40@gmail.com>
2007-12-28 00:50:57 +0000

    Vector 1.0 done
    git-svn-id: https://svn.code.sf.net/p/gjava/code@795 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- G-Java/src/org/gcreator/compilers/gjava/api/Vector.java
+++ G-Java/src/org/gcreator/compilers/gjava/api/Vector.java
@@ -2,13 +2,267 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.gcreator.compilers.gjava.api;
 
 /**
- *
- * @author Ali
+ * The {@code Vector} class implements a growable array of
+ * objects. Like an array, it contains components that can be
+ * accessed using an integer index. However, the size of a
+ * {@code Vector} can grow or shrink as needed to accommodate
+ * adding and removing items after the {@code Vector} has been created.
+ * @author TGMG
+ * @version 1.0
  */
 public class Vector {
 
+    java.util.Vector source;
+
+    /**
+     * Constructs an empty vector so that its internal data array
+     * has size {@code 10}.
+     */
+    Vector() {
+        source = new java.util.Vector();
+    }
+
+    /**
+     * Constructs an empty vector with the specified initial capacity
+     */
+    Vector(int size) {
+        source = new java.util.Vector(size);
+    }
+
+    /**
+     * Appends the specified element to the end of this Vector.
+     *
+     * @param o element to be appended to this Vector
+     * 
+     */
+    public void add(Object o) {
+        source.add(o);
+    }
+    
+   
+    /**
+     * Inserts the specified element at the specified position in this Vector.
+     * Shifts the element currently at that position (if any) and any
+     * subsequent elements to the right (adds one to their indices).
+     *
+     * @param index index at which the specified element is to be inserted
+     * @param o element to be inserted
+     */
+    public void add(int index, Object o)
+    {
+        source.add(index, o);
+    }
+    
+    /**
+     * Removes all of the elements from this Vector.  The Vector will
+     * be empty after this call returns.
+     */
+    public void clear(){
+        source.clear();
+    }
+    
+    @Override
+    public Object clone()
+    {
+       return source.clone();
+    }
+    
+     /**
+     * Returns {@code true} if this vector contains the specified element.
+     * More formally, returns {@code true} if and only if this vector
+     * contains at least one element {@code e} such that
+     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+     *
+     * @param o element whose presence in this vector is to be tested
+     * @return {@code true} if this vector contains the specified element
+     */
+    public boolean contains(Object o) {
+	return source.contains(o);
+    }
+    
+    /**
+     * Returns the first component (the item at index {@code 0}) of
+     * this vector.
+     *
+     * @return     the first component of this vector
+     */
+    public Object firstElement() {
+        return source.firstElement();
+    }
+    
+    /**
+     * Returns the element at the specified position in this Vector.
+     *
+     * @param index index of the element to return
+     * @return object at the specified index
+     * @throws ArrayIndexOutOfBoundsException if the index is out of range
+     *            ({@code index < 0 || index >= size()})
+     */
+    public  Object get(int index) {
+        return source.get(index);
+    }
+    
+     /**
+     * Returns the index of the first occurrence of the specified element
+     * in this vector, or -1 if this vector does not contain the element.
+     * More formally, returns the lowest index {@code i} such that
+     * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>,
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @return the index of the first occurrence of the specified element in
+     *         this vector, or -1 if this vector does not contain the element
+     */
+    public int indexOf(Object o) {
+        return source.indexOf(o);
+    }
+    
+    /**
+     * Returns the index of the first occurrence of the specified element in
+     * this vector, searching forwards from {@code index}, or returns -1 if
+     * the element is not found.
+     * More formally, returns the lowest index {@code i} such that
+     * <tt>(i&nbsp;&gt;=&nbsp;index&nbsp;&amp;&amp;&nbsp;(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i))))</tt>,
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @param index index to start searching from
+     * @return the index of the first occurrence of the element in
+     *         this vector at position {@code index} or later in the vector;
+     *         {@code -1} if the element is not found.
+     */
+    public int indexOf(Object o, int index) {
+        return source.indexOf(o, index);
+    }
+    
+    /**
+     * Tests if this vector has no components.
+     *
+     * @return  {@code true} if and only if this vector has
+     *          no components, that is, its size is zero;
+     *          {@code false} otherwise.
+     */
+    public  boolean isEmpty() {
+        return source.isEmpty();
+    }
+    
+    /**
+     * Returns the last component of the vector.
+     *
+     * @return  the last component of the vector, i.e., the component at index
+     *          <code>size()&nbsp;-&nbsp;1</code>.
+     */
+    public Object lastElement() {
+        return source.lastElement();
+    }
+    
+    /**
+     * Returns the index of the last occurrence of the specified element
+     * in this vector, or -1 if this vector does not contain the element.
+     * More formally, returns the highest index {@code i} such that
+     * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>,
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @return the index of the last occurrence of the specified element in
+     *         this vector, or -1 if this vector does not contain the element
+     */
+    public int lastIndexOf(Object o) {
+        return source.lastIndexOf(o);
+    }
+    
+    /**
+     * Returns the index of the last occurrence of the specified element in
+     * this vector, searching backwards from {@code index}, or returns -1 if
+     * the element is not found.
+     * More formally, returns the highest index {@code i} such that
+     * <tt>(i&nbsp;&lt;=&nbsp;index&nbsp;&amp;&amp;&nbsp;(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i))))</tt>,
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @param index index to start searching backwards from
+     * @return the index of the last occurrence of the element at position
+     *         less than or equal to {@code index} in this vector;
+     *         -1 if the element is not found.
+     * @throws IndexOutOfBoundsException if the specified index is greater
+     *         than or equal to the current size of this vector
+     */
+    public synchronized int lastIndexOf(Object o, int index) {
+        return source.lastIndexOf(o, index);
+    }
+    
+     /**
+     * Removes the first occurrence of the specified element in this Vector
+     * If the Vector does not contain the element, it is unchanged.  More
+     * formally, removes the element with the lowest index i such that
+     * {@code (o==null ? get(i)==null : o.equals(get(i)))} (if such
+     * an element exists).
+     *
+     * @param o element to be removed from this Vector, if present
+     * @return true if the Vector contained the specified element
+     */
+    public boolean remove(Object o) {
+        return source.remove(o);
+    }
+    
+    /**
+     * Removes the element at the specified position in this Vector.
+     * Shifts any subsequent elements to the left (subtracts one from their
+     * indices).  Returns the element that was removed from the Vector.
+     *
+     * @throws ArrayIndexOutOfBoundsException if the index is out of range
+     *         ({@code index < 0 || index >= size()})
+     * @param index the index of the element to be removed
+     * @return element that was removed
+     * @since 1.2
+     */
+    public Object remove(int index) {
+        return source.remove(index);
+    }
+
+    /**
+     * Removes all components from this vector and sets its size to zero.
+     */
+    public void removeAll() {
+        source.removeAllElements();
+    }
+    
+    /**
+     * Replaces the element at the specified position in this Vector with the
+     * specified element.
+     *
+     * @param index index of the element to replace
+     * @param element element to be stored at the specified position
+     * @return the element previously at the specified position
+     * @throws ArrayIndexOutOfBoundsException if the index is out of range
+     *	       ({@code index < 0 || index >= size()})
+     */
+    public Object set(int index, Object element) {
+        return source.set(index, element);
+    }
+    
+    /**
+     * Sets the size of this vector. If the new size is greater than the
+     * current size, new {@code null} items are added to the end of
+     * the vector. If the new size is less than the current size, all
+     * components at index {@code newSize} and greater are discarded.
+     *
+     * @param  newSize   the new size of this vector
+     * @throws ArrayIndexOutOfBoundsException if the new size is negative
+     */
+    public void setSize(int newSize) {
+        source.setSize(newSize);
+    }
+    
+    /**
+     * Returns the number of components in this vector.
+     *
+     * @return  the number of components in this vector
+     */
+    public int getSize() {
+        return source.size();
+    }
 }
