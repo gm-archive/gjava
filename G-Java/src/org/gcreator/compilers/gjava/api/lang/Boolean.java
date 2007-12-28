fe37a8225757728c03b04a711ad5be5bc4d96808
tgmg <amorri40@gmail.com>
2007-12-28 22:38:18 +0000

    Updated Boolean to 1.0
    git-svn-id: https://svn.code.sf.net/p/gjava/code@803 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- G-Java/src/org/gcreator/compilers/gjava/api/lang/Boolean.java
+++ G-Java/src/org/gcreator/compilers/gjava/api/lang/Boolean.java
@@ -1,14 +1,48 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package org.gcreator.compilers.gjava.api.lang;
 
 /**
- *
- * @author Ali
+ * The Boolean class wraps a value of the primitive type boolean in an object.
+ * In addition, this class provides many methods for converting a boolean to a String and a String to a boolean, as well as other constants and methods useful when dealing with a boolean.
+ * @author TGMG
  */
 public class Boolean {
+private boolean b;
+
+/**
+ * The Boolean object corresponding to the primitive value true.
+ */
+public static Boolean TRUE = new Boolean(true);
+/**
+ * The Boolean object corresponding to the primitive value false.
+ */
+public static Boolean FALSE = new Boolean(false);
+
+Boolean(boolean bb)
+{
+    b=bb;
+}
+
+/**
+ * Returns the value of this Boolean object as a boolean primitive.
+ * @return
+ */
+public boolean getPrimitive()
+{
+    return b;
+}
+
+/**
+ * Returns a Boolean with a value represented by the specified String. The Boolean returned represents the value true if the string argument is not null  and is equal, ignoring case, to the string "true".
+ * @param s
+ * @return
+ */
+public boolean parseBoolean(java.lang.String s)
+{
+    return java.lang.Boolean.valueOf(s).booleanValue();
+    
+}
+
 
 }
--- G-Java/src/org/gcreator/compilers/gjava/api/lang/String.java
+++ G-Java/src/org/gcreator/compilers/gjava/api/lang/String.java
@@ -11,8 +11,24 @@ import org.gcreator.compilers.gjava.api.*;
  *
  * @author TGMG
  */
-public class String extends Object{
+public class String extends org.gcreator.compilers.gjava.api.Object{
     
+   java.lang.String s="";
+   
+   /**
+ * Returns the value of this String object as a string primitive.
+ * @return
+ */
+public java.lang.String getPrimitive()
+{
+    return s;
+}
+
+    @Override
+    public java.lang.String toString() {
+        return s;
+    }
+   
    
 
 }
