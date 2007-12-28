e95187b76fbb28788c6efc7dba434f99c3dc1aaa
tgmg <amorri40@gmail.com>
2007-12-28 22:07:20 +0000

    Added primative wrappers
    git-svn-id: https://svn.code.sf.net/p/gjava/code@802 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ G-Java/src/org/gcreator/compilers/gjava/api/lang/Long.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.gcreator.compilers.gjava.api.lang;
+
+/**
+ *
+ * @author Ali
+ */
+public class Long {
+
+}
---
+++ G-Java/src/org/gcreator/compilers/gjava/api/lang/String.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.gcreator.compilers.gjava.api.lang;
+
+import org.gcreator.compilers.gjava.api.*;
+
+/**
+ *
+ * @author TGMG
+ */
+public class String extends Object{
+    
+   
+
+}
