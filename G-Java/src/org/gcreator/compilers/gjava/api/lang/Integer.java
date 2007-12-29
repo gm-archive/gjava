cf80e5537abf5db2159b881d938bc5d6eec8dc57
tgmg <amorri40@gmail.com>
2007-12-29 01:47:50 +0000

    Updated Integer to 1.0
    git-svn-id: https://svn.code.sf.net/p/gjava/code@807 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- G-Java/src/org/gcreator/compilers/gjava/api/lang/Integer.java
+++ G-Java/src/org/gcreator/compilers/gjava/api/lang/Integer.java
@@ -6,9 +6,40 @@
 package org.gcreator.compilers.gjava.api.lang;
 
 /**
- *
- * @author Ali
+ * The Integer class wraps a value of the primitive type int  in an object. 
+ * In addition, this class provides several methods for converting an int to a String and a String to an int, as well as other constants and methods useful when dealing with an int.
+ * @author TGMG
  */
 public class Integer {
+    
+    int i;
+/**
+     * The largest positive finite value of type float.
+     */
+    public static float MAX_VALUE = java.lang.Float.MAX_VALUE;
+    /**
+     * The smallest positive value of type float.
+     */
+    public static float MIN_VALUE = java.lang.Float.MIN_VALUE;
+    
+    /**
+     * Returns the value of this Float object as a char primitive.
+     * @return
+     */
+    public int getPrimitive() {
+        return i;
+    }
+
+         
+         
+
+    /**
+     * Returns a new double initialized to the value represented by the specified String, as performed by the valueOf  method of class Double.
+     * @param s
+     * @return
+     */
+    public static int parseString(java.lang.String s) {
+        return java.lang.Integer.parseInt(s);
+    }
 
 }
