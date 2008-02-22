ac152def041387c9a412f3d79af0d5e785dcbda4
luiscubal <luiscubal@gmail.com>
2008-02-22 23:01:05 +0000

    Small fix.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@901 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- Aurora/src/org/gcreator/components/popupmenus/AutocompleteFrame.java
+++ Aurora/src/org/gcreator/components/popupmenus/AutocompleteFrame.java
@@ -20,35 +20,5 @@ public class AutocompleteFrame extends JFrame{
     public AutocompleteFrame(){
         super();
         setUndecorated(true);
-        addFocusListener(new FocusListener(){
-            public void focusGained(FocusEvent evt){}
-            public void focusLost(FocusEvent evt){
-                dispose();
-            }
-        });
-        JPanel p = new JPanel() {
-
-            public Image getBgImage() {
-                Image background = null;
-                try {
-                    Robot rbt = new Robot();
-                    Toolkit tk = Toolkit.getDefaultToolkit();
-                    Dimension dim = tk.getScreenSize();
-                    background = rbt.createScreenCapture(
-                            new Rectangle(0, 0, (int) dim.getWidth(),
-                            (int) dim.getHeight()));
-                } catch (Exception ex) {}
-                return background;
-            }
-
-            public void paint(Graphics g) {
-                Point pos = this.getLocationOnScreen();
-                Point offset = new Point(-pos.x, -pos.y);
-                g.drawImage(getBgImage(), offset.x, offset.y, null);
-                super.paintChildren(g);
-
-            }
-        };
-        this.setContentPane(p);
     }
 }
