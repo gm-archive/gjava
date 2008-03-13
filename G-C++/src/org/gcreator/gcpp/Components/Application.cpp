74b44bc4abd3526194a1eb09bfc523e9b05041b7
sleija <fazzitron@gmail.com>
2008-03-13 12:59:40 +0000

    git-svn-id: https://svn.code.sf.net/p/gjava/code@950 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- G-C++/src/org/gcreator/gcpp/Components/Application.cpp
+++ G-C++/src/org/gcreator/gcpp/Components/Application.cpp
@@ -6,8 +6,20 @@
 org::gcreator::Components::Application::Application(int _argc, char** _argv){
 	this->argc = argc;
 	this->argv = argv;
-	while(!key[KEY_ESC])
+	while(!org::gcreator::Interaction::Keyboard::isKeyPressed(SDLK_ESCAPE))
 		;
+}
+
+org::gcreator::Components::Application::gameInit(int W, int H, int BPP, String caption="Game"){
+        SDL_Surface *screen
+        if(SDL_Init(SDL_INIT_EVERYTHING)==-1){
+            return 1;
+        }
+        screen=SDL_SetVideoMode(W,H,BPP,SDL_SWSURFACE);
+        if(screen==NULL){
+            return 1;
+        }
+        SDL_WM_SetCaption(caption, NULL)
 }
 
 org::gcreator::Components::Application* Game;
