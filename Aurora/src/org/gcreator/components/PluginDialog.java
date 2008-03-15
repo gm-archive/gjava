a13bd4700f495a844ac63050ea772a5fe5ddd1f1
luiscubal <luiscubal@gmail.com>
2008-03-15 13:38:24 +0000

    Getting ready for PluginDialog changes.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@954 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- Aurora/src/org/gcreator/components/PluginDialog.java
+++ Aurora/src/org/gcreator/components/PluginDialog.java
@@ -28,6 +28,8 @@ public class PluginDialog extends javax.swing.JDialog {
             System.out.println("Plugin: " + plugin.name);
             list.addElement(plugin.name, plugin.image, plugin);
         }
+        pack();
+        setMinimumSize(getSize());
     }
     
     /** This method is called from within the constructor to
@@ -40,20 +42,35 @@ public class PluginDialog extends javax.swing.JDialog {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Plugin Manager");
+        setResizable(false);
         getContentPane().add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        jButton1.setText("Install new plugin");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 214, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 194, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 317, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1)
+                .addContainerGap(283, Short.MAX_VALUE))
         );
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.WEST);
@@ -61,7 +78,12 @@ public class PluginDialog extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
