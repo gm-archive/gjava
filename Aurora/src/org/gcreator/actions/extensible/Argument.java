3c8e354026c6ada105655fc8516af7b7b2b81ed0
luiscubal <luiscubal@gmail.com>
2008-03-31 21:32:26 +0000

    Initial work with custom actions
    git-svn-id: https://svn.code.sf.net/p/gjava/code@1021 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ Aurora/src/org/gcreator/actions/extensible/Argument.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.gcreator.actions.extensible;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Luís
+ */
+public interface Argument {
+    public JComponent generateComponent(int index);
+}
---
+++ Aurora/src/org/gcreator/actions/extensible/TextArgument.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.gcreator.actions.extensible;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Luís
+ */
+public class TextArgument implements Argument{
+    public TextArgument(){
+        
+    }
+
+    public JComponent generateComponent(int index){
+        return null;
+    }
+}
