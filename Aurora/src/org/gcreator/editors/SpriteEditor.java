6e1286f8b42ab0e6383ae344873d8efedb7e0f45
luiscubal <luiscubal@gmail.com>
2008-05-21 20:08:47 +0000

    Sprite Editor no longer asks the user to save if no modification was done. Very small UI changes to the sprite editor.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@1252 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- Aurora/src/org/gcreator/editors/SpriteEditor.java
+++ Aurora/src/org/gcreator/editors/SpriteEditor.java
@@ -32,6 +32,7 @@ public class SpriteEditor extends TabPanel {
     public SpriteEditor(org.gcreator.fileclass.GFile file,Project project) {
         this.project = project;
         this.file = file;
+        boolean dontChange = false;
         //title = file.name + "(" + project.name+")";
         //if (file.value instanceof Sprite)
         //{
@@ -40,9 +41,14 @@ public class SpriteEditor extends TabPanel {
                 Sprite spr = new Sprite();
                 spr.readXml((String) file.value);
                 file.value = spr;
+                changed = true;
             }
-            else
+            else{
+                System.out.println("GOT HERE");
                 this.sprite = (Sprite) ((Sprite) file.value).clone();
+                changed = false;
+                dontChange = true;
+            }
         }
         else{
             this.sprite = (Sprite) new Sprite();
@@ -93,6 +99,10 @@ public class SpriteEditor extends TabPanel {
                 updateName();
             }
         });
+        if(dontChange){
+            System.out.println("And here");
+            changed = false;
+        }
     }
     
     public void updateName() {
@@ -101,6 +111,11 @@ public class SpriteEditor extends TabPanel {
         org.gcreator.core.gcreator.panel.workspace.updateUI();
     }
      
+    public boolean canSave(){
+        System.out.println("canSave: " + changed);
+        return changed;
+    }
+    
     @Override
     public boolean Save() {
         changed = false;
@@ -122,6 +137,7 @@ public class SpriteEditor extends TabPanel {
      
     @Override
     public boolean wasModified() {
+        System.out.println("wasModified: " + changed);
         return changed;
     }
     
@@ -182,6 +198,8 @@ public class SpriteEditor extends TabPanel {
         jSpinner6 = new javax.swing.JSpinner();
         jButton8 = new javax.swing.JButton();
         jButton9 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
         jCheckBox1 = new javax.swing.JCheckBox();
         jPanel6 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
@@ -262,10 +280,10 @@ public class SpriteEditor extends TabPanel {
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jPanel13, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 191, Short.MAX_VALUE)
-            .add(jButton2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 191, Short.MAX_VALUE)
+            .add(jPanel13, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
+            .add(jButton2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
             .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel4Layout.createSequentialGroup()
-                .add(jLabel2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 89, Short.MAX_VALUE)
+                .add(jLabel2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jLabel3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 84, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -281,12 +299,12 @@ public class SpriteEditor extends TabPanel {
                 .add(2, 2, 2)
                 .add(jPanel4Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel4Layout.createSequentialGroup()
-                        .add(jButton1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
+                        .add(jButton1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 191, Short.MAX_VALUE)
                         .add(2, 2, 2))
                     .add(jPanel4Layout.createSequentialGroup()
                         .add(jPanel4Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                             .add(jLabel5)
-                            .add(jLabel4, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE))
+                            .add(jLabel4, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 181, Short.MAX_VALUE))
                         .addContainerGap())))
         );
         jPanel4Layout.setVerticalGroup(
@@ -301,7 +319,7 @@ public class SpriteEditor extends TabPanel {
                     .add(jLabel2))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jLabel4)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 78, Short.MAX_VALUE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 136, Short.MAX_VALUE)
                 .add(jLabel5)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jPanel4Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
@@ -349,7 +367,7 @@ public class SpriteEditor extends TabPanel {
             }
         });
 
-        jButton8.setText("Automatic");
+        jButton8.setText("Automatic (Strict)");
 
         jButton9.setText("Full Image");
         jButton9.addActionListener(new java.awt.event.ActionListener() {
@@ -357,6 +375,10 @@ public class SpriteEditor extends TabPanel {
                 jButton9ActionPerformed(evt);
             }
         });
+
+        jButton5.setText("Automatic (Standard)");
+
+        jButton7.setText("Automatic (Custom)");
 
         org.jdesktop.layout.GroupLayout jPanel2Layout = new org.jdesktop.layout.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -373,12 +395,14 @@ public class SpriteEditor extends TabPanel {
                             .add(jLabel10))
                         .add(31, 31, 31)
                         .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(jSpinner6, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
-                            .add(jSpinner4, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
-                            .add(jSpinner3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
-                            .add(jSpinner5, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)))
-                    .add(jButton8, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 148, Short.MAX_VALUE)
-                    .add(org.jdesktop.layout.GroupLayout.TRAILING, jButton9, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 148, Short.MAX_VALUE))
+                            .add(jSpinner6, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)
+                            .add(jSpinner4, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)
+                            .add(jSpinner3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)
+                            .add(jSpinner5, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)))
+                    .add(jButton8, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
+                    .add(jButton5, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(jButton7, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
+                    .add(jButton9, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -386,8 +410,12 @@ public class SpriteEditor extends TabPanel {
             .add(jPanel2Layout.createSequentialGroup()
                 .add(jButton8)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jButton5)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jButton7)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jButton9)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 28, Short.MAX_VALUE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(jLabel10)
                     .add(jSpinner4, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
@@ -412,11 +440,6 @@ public class SpriteEditor extends TabPanel {
                 jCheckBox1ActionPerformed(evt);
             }
         });
-        jCheckBox1.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent evt) {
-                jCheckBox1PropertyChange(evt);
-            }
-        });
 
         org.jdesktop.layout.GroupLayout jPanel5Layout = new org.jdesktop.layout.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
@@ -434,7 +457,7 @@ public class SpriteEditor extends TabPanel {
             .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel5Layout.createSequentialGroup()
                 .addContainerGap()
                 .add(jPanel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 34, Short.MAX_VALUE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 44, Short.MAX_VALUE)
                 .add(jCheckBox1)
                 .addContainerGap())
         );
@@ -496,7 +519,7 @@ public class SpriteEditor extends TabPanel {
                         .add(12, 12, 12)
                         .add(jLabel7, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 18, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(jSpinner2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)))
+                        .add(jSpinner2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -525,7 +548,7 @@ public class SpriteEditor extends TabPanel {
             .add(jPanel6Layout.createSequentialGroup()
                 .addContainerGap()
                 .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(227, Short.MAX_VALUE))
+                .addContainerGap(285, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Other", jPanel6);
@@ -552,14 +575,14 @@ public class SpriteEditor extends TabPanel {
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
                     .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
                         .addContainerGap()
-                        .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE))
+                        .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 446, Short.MAX_VALUE))
                     .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
                         .add(12, 12, 12)
                         .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                             .add(jLabel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 17, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                             .add(jTextField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 20, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(jTabbedPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE)))
+                        .add(jTabbedPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -571,60 +594,65 @@ public class SpriteEditor extends TabPanel {
 
     private void jSpinner1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jSpinner1MouseClicked
       changed=true;
+      System.out.println("1");
     }//GEN-LAST:event_jSpinner1MouseClicked
 
     private void jSpinner1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jSpinner1KeyTyped
         changed=true;
+        System.out.println("2");
     }//GEN-LAST:event_jSpinner1KeyTyped
 
     private void jSpinner2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jSpinner2MouseClicked
         changed=true;
+        System.out.println("3");
     }//GEN-LAST:event_jSpinner2MouseClicked
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
         changed=true;
         sprite.precise = jCheckBox1.isSelected();
+        System.out.println("4");
     }//GEN-LAST:event_jCheckBox1ActionPerformed
-
-    private void jCheckBox1PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jCheckBox1PropertyChange
-        changed=true;
-        sprite.precise = jCheckBox1.isSelected();
-    }//GEN-LAST:event_jCheckBox1PropertyChange
 
     private void jSpinner1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner1StateChanged
         changed=true;
         sprite.originX = (Integer) jSpinner1.getValue();
         jScrollPane1.updateUI();
+        System.out.println("6");
     }//GEN-LAST:event_jSpinner1StateChanged
 
     private void jSpinner2StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner2StateChanged
         changed=true;
         sprite.originY = (Integer) jSpinner2.getValue();
         jScrollPane1.updateUI();
+        System.out.println("7");
     }//GEN-LAST:event_jSpinner2StateChanged
 
     private void jSpinner4StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner4StateChanged
         changed=true;
         sprite.BBTop = (Integer) jSpinner4.getValue();
         jScrollPane1.updateUI();
+        System.out.println("8");
     }//GEN-LAST:event_jSpinner4StateChanged
 
     private void jSpinner6StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner6StateChanged
         changed=true;
         sprite.BBBottom = (Integer) jSpinner6.getValue();
         jScrollPane1.updateUI();
+        System.out.println("9");
     }//GEN-LAST:event_jSpinner6StateChanged
 
     private void jSpinner5StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner5StateChanged
         changed=true;
         sprite.BBRight = (Integer) jSpinner5.getValue();
         jScrollPane1.updateUI();
+        System.out.println("10");
     }//GEN-LAST:event_jSpinner5StateChanged
 
     private void jSpinner3StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner3StateChanged
         changed=true;
         sprite.BBleft = (Integer) jSpinner3.getValue();
         jScrollPane1.updateUI();
+        System.out.println("11");
     }//GEN-LAST:event_jSpinner3StateChanged
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
@@ -633,6 +661,7 @@ public class SpriteEditor extends TabPanel {
         jSpinner2.setValue(sprite.height / 2);
         sprite.originX = (Integer) jSpinner1.getValue();
         sprite.originY = (Integer) jSpinner2.getValue();
+        System.out.println("12");
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
@@ -645,6 +674,7 @@ public class SpriteEditor extends TabPanel {
         sprite.BBTop = (Integer) jSpinner4.getValue();
         sprite.BBRight = (Integer) jSpinner5.getValue();
         sprite.BBBottom = (Integer) jSpinner6.getValue();
+        System.out.println("13");
     }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -671,6 +701,7 @@ public class SpriteEditor extends TabPanel {
             System.out.println("Image not right size!");
         jLabel4.setText("Subimages:"+sprite.countImages());
         jScrollPane1.updateUI();
+        System.out.println("14");
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -681,6 +712,7 @@ public class SpriteEditor extends TabPanel {
             id = 0;
         jLabel4.setText("Subimages: " + sprite.countImages());
         setViewedId(id);
+        System.out.println("15");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
@@ -709,6 +741,7 @@ public class SpriteEditor extends TabPanel {
         jSpinner1.setValue(x);
         jSpinner2.setValue(y);
         prev.repaint();     
+        System.out.println("16");
     }//GEN-LAST:event_jScrollPane1MouseDragged
 
     private void jScrollPane1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jScrollPane1MousePressed
@@ -717,11 +750,13 @@ public class SpriteEditor extends TabPanel {
             return;
         dragging = true;
         jScrollPane1MouseDragged(evt);
+        System.out.println("17");
     }//GEN-LAST:event_jScrollPane1MousePressed
 
     private void jScrollPane1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jScrollPane1MouseReleased
         changed = true;
         dragging = false;
+        System.out.println("18");
     }//GEN-LAST:event_jScrollPane1MouseReleased
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
@@ -773,8 +808,10 @@ public class SpriteEditor extends TabPanel {
             for (int j = 0; j < sprite.height; j++) {
                 int RGBA = bufImg.getRGB(i, j);
                 Color col = new Color(RGBA,true);
-            }
+        }
         */
+        System.out.println("19");
+        
     } 
     
     // <editor-fold defaultstate="collapsed" desc="Gnerated Variables"> 
@@ -783,7 +820,9 @@ public class SpriteEditor extends TabPanel {
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JButton jButton9;
     private javax.swing.JCheckBox jCheckBox1;
