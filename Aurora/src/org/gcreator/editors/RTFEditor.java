43666b805e9f943cda0fe76af3e87600c1a95ed2
luiscubal <luiscubal@gmail.com>
2008-07-30 12:52:51 +0000

    RTF Editor now supports italic, underline and foreground font.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@1478 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- Aurora/src/org/gcreator/editors/RTFEditor.java
+++ Aurora/src/org/gcreator/editors/RTFEditor.java
@@ -6,6 +6,8 @@
 
 package org.gcreator.editors;
 
+import com.l2fprod.common.swing.JFontChooser;
+import java.awt.Font;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import javax.swing.event.CaretEvent;
@@ -48,17 +50,37 @@ public class RTFEditor extends TabPanel {
                 updateBtn();
             }
         });
+        updateBtn();
     }
     
     private void updateBtn(){
         int sel = jEditorPane1.getSelectionStart();
-        jButton1.setSelected(getProperty(sel, StyleConstants.Bold));
+        jButton1.setSelected(getBoolProperty(sel, StyleConstants.Bold));
+        jButton2.setSelected(getBoolProperty(sel, StyleConstants.Italic));
+        jButton3.setSelected(getBoolProperty(sel, StyleConstants.Underline));
+        Font f = new Font(getStringProperty(sel, StyleConstants.FontFamily),
+                getBoolProperty(sel, StyleConstants.Bold)?Font.BOLD:0
+                | (getBoolProperty(sel, StyleConstants.Italic)?Font.ITALIC:0),
+                getIntProperty(sel, StyleConstants.FontSize));
+        jButton4.setFont(f);
     }
     
-    public boolean getProperty(int index, Object prop){
+    public boolean getBoolProperty(int index, Object prop){
         StyledDocument d = (StyledDocument) jEditorPane1.getDocument();
         Object o = d.getCharacterElement(index).getAttributes().getAttribute(prop);
         return o!=null&&o instanceof Boolean ? (Boolean) o : false;
+    }
+    
+    public String getStringProperty(int index, Object prop){
+        StyledDocument d = (StyledDocument) jEditorPane1.getDocument();
+        Object o = d.getCharacterElement(index).getAttributes().getAttribute(prop);
+        return o!=null&&o instanceof String ? (String) o : null;
+    }
+    
+    public Integer getIntProperty(int index, Object prop){
+        StyledDocument d = (StyledDocument) jEditorPane1.getDocument();
+        Object o = d.getCharacterElement(index).getAttributes().getAttribute(prop);
+        return o!=null&&o instanceof Integer ? (Integer) o : null;
     }
 
     public boolean wasModified(){
@@ -95,6 +117,9 @@ public class RTFEditor extends TabPanel {
         jEditorPane1 = new javax.swing.JEditorPane();
         jToolBar1 = new javax.swing.JToolBar();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setLayout(new java.awt.BorderLayout());
 
@@ -119,6 +144,40 @@ public class RTFEditor extends TabPanel {
         });
         jToolBar1.add(jButton1);
 
+        jButton2.setFont(jButton2.getFont().deriveFont((jButton2.getFont().getStyle() | java.awt.Font.ITALIC)));
+        jButton2.setText("I");
+        jButton2.setFocusable(false);
+        jButton2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButton2.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jToolBar1.add(jButton2);
+
+        jButton3.setText("<html><u>U");
+        jButton3.setFocusable(false);
+        jButton3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButton3.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jToolBar1.add(jButton3);
+
+        jButton4.setText("Font");
+        jButton4.setFocusable(false);
+        jButton4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButton4.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jToolBar1.add(jButton4);
+
         add(jToolBar1, java.awt.BorderLayout.PAGE_START);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -133,9 +192,53 @@ private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
     changed = true;
 }//GEN-LAST:event_jButton1ActionPerformed
 
+private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    StyledDocument d = (StyledDocument) jEditorPane1.getDocument();
+    int start = jEditorPane1.getSelectionStart();
+    int end = jEditorPane1.getSelectionEnd();
+    SimpleAttributeSet s = new SimpleAttributeSet();
+    s.addAttribute(StyleConstants.Italic, !jButton2.isSelected());
+    d.setCharacterAttributes(start, end-start, s, false);
+    updateBtn();
+    changed = true;
+}//GEN-LAST:event_jButton2ActionPerformed
+
+private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    StyledDocument d = (StyledDocument) jEditorPane1.getDocument();
+    int start = jEditorPane1.getSelectionStart();
+    int end = jEditorPane1.getSelectionEnd();
+    SimpleAttributeSet s = new SimpleAttributeSet();
+    s.addAttribute(StyleConstants.Underline, !jButton3.isSelected());
+    d.setCharacterAttributes(start, end-start, s, false);
+    updateBtn();
+    changed = true;
+}//GEN-LAST:event_jButton3ActionPerformed
+
+private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    JFontChooser fc = new JFontChooser();
+    fc.setSelectedFont(jButton4.getFont());
+    Font f = fc.showFontDialog(this, "F");
+    if(f!=null){
+        StyledDocument d = (StyledDocument) jEditorPane1.getDocument();
+        int start = jEditorPane1.getSelectionStart();
+        int end = jEditorPane1.getSelectionEnd();
+        SimpleAttributeSet s = new SimpleAttributeSet();
+        s.addAttribute(StyleConstants.FontFamily, f.getFamily());
+        s.addAttribute(StyleConstants.FontSize, f.getSize());
+        s.addAttribute(StyleConstants.Bold, f.isBold());
+        s.addAttribute(StyleConstants.Italic, f.isItalic());
+        d.setCharacterAttributes(start, end-start, s, false);
+        updateBtn();
+        changed = true;
+    }
+}//GEN-LAST:event_jButton4ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JEditorPane jEditorPane1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JToolBar jToolBar1;
