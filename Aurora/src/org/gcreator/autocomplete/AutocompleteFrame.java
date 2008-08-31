a9f5f8a3cb8a028e8814220a65fff4b29b07105d
luiscubal <luiscubal@gmail.com>
2008-08-31 11:41:23 +0000

    Initial prototype of the previewer.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@1514 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- Aurora/src/org/gcreator/autocomplete/AutocompleteFrame.java
+++ Aurora/src/org/gcreator/autocomplete/AutocompleteFrame.java
@@ -11,35 +11,42 @@ package org.gcreator.autocomplete;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import javax.swing.*;
 import org.gcreator.components.uiplus.DialogPlus;
 
 /**
  * @author Luís Reis
  */
-public class AutocompleteFrame extends DialogPlus{
+public class AutocompleteFrame extends DialogPlus {
+
     protected JList list;
     protected JScrollPane scroll;
     protected JEditorPane doc;
-    
-    public AutocompleteFrame(String text){
+
+    public AutocompleteFrame(String text) {
         super();
         setUndecorated(true);
-        addFocusListener(new FocusListener(){
-            public void focusLost(FocusEvent evt){
+        addFocusListener(new FocusListener() {
+
+            public void focusLost(FocusEvent evt) {
                 dispose();
             }
-            
-            public void focusGained(FocusEvent evt){}
+
+            public void focusGained(FocusEvent evt) {
+            }
         });
-        setSize(350, 180);
+        setSize(450, 180);
         setLayout(new BorderLayout());
         JLabel label = new JLabel(text);
         label.setFocusable(false);
         label.setVisible(true);
         add(BorderLayout.NORTH, label);
         setTitle(text);
-        
+
         scroll = new JScrollPane();
         scroll.setFocusable(false);
         scroll.setVisible(true);
@@ -53,19 +60,52 @@ public class AutocompleteFrame extends DialogPlus{
         doc.setVisible(true);
         doc.setEditable(false);
         doc.setContentType("text/html");
-        JScrollPane p = new JScrollPane(){
-            public Dimension getPreferredSize(){
+        JScrollPane p = new JScrollPane() {
+
+            public Dimension getPreferredSize() {
                 Dimension d = super.getPreferredSize();
-                d.width = 180;
+                d.width = 230;
                 return d;
             }
         };
+
+        doc.setText("Loading...");
+        
+        Thread t = new Thread() {
+
+            public void run() {
+                try {
+                    URL url = new URL("http://wiki.g-creator.org/doku.php?id=show_message");
+                    URLConnection connection = url.openConnection();
+                    BufferedReader inStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String inputLine = "";
+                    String txt = "";
+                    boolean reading = false;
+                    while (null != (inputLine = inStream.readLine())) {
+                        if (inputLine.equals("    <!-- wikipage start -->")) {
+                            reading = true;
+                        } else if (inputLine.equals("    <!-- wikipage stop -->")) {
+                            reading = false;
+                        }
+                        if (reading) {
+                            txt += inputLine;
+                        }
+                    }
+                    doc.setText(txt);
+                    inStream.close();
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
+            }
+        };
+        t.start();
+
         p.setVisible(true);
         p.setViewportView(doc);
         add(BorderLayout.LINE_END, p);
     }
-    
-    public boolean requestDie(){
+
+    public boolean requestDie() {
         return false;
     }
 }
