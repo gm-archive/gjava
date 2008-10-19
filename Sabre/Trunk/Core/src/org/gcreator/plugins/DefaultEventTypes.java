f5eed009d4fb7502655124c5dfa9b26740a55981
bobserge <bobtheblueberry@gmail.com>
2008-10-19 02:29:01 +0000

    Added some default event types and configured the license. Please go to Tools>Templates and select the "Licenses" folder and click "Add", then select license-mit.txt.
    git-svn-id: https://svn.code.sf.net/p/gjava/code@1547 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

---
+++ Sabre/Trunk/Core/src/org/gcreator/plugins/DefaultEventTypes.java
@@ -0,0 +1,68 @@
+/*
+Copyright (C) 2008 Luís Reis<luiscubal@gmail.com>
+Copyright (C) 2008 Serge Humphrey <bob@bobtheblueberry.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+package org.gcreator.plugins;
+
+/**
+ * Defines some default event types.
+ * Use these to avoid the use of string literals.
+ * 
+ * @author Serge Humphrey
+ */
+public class DefaultEventTypes {
+    
+    /** Don't allow instantation
+     */
+    private DefaultEventTypes() {
+    }
+    
+    /**
+     * An undefiend event type.
+     */
+    public static final String UNDEFINED = "undefined";
+    
+    /**
+     * When a file changes.
+     */
+    public static final String FILE_CHANGED = "file-changed";
+    
+    /**
+     * When the application is initilized.
+     */
+    public static final String APPLICATION_INITIALIZED = "application-initilized";
+    
+    /**
+     * When the window is created.
+     */
+    public static final String WINDOW_CREATED = "window-created";
+    
+    /**
+     * When the window is disposed
+     */
+    public static final String WINDOW_DISPOSE = "window-dispose";
+    
+    /**
+     * All events.
+     */
+    public static final String ALL = "all";
+}
