d0822c1a7e8aa859aa661361d3c63b2d7633ee4b
tgmg <amorri40@gmail.com>
2008-10-22 13:33:01 +0000

    Now exports the runner file at start of plugin
    git-svn-id: https://svn.code.sf.net/p/gjava/code@1569 0bf6c8e7-452b-0410-8c87-9aaedaf074f6

--- Dolphin/src/org/lgj/DolphinWriter.java
+++ Dolphin/src/org/lgj/DolphinWriter.java
@@ -1,9 +1,16 @@
 package org.lgj;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.lateralgm.file.GmFile;
 import org.lateralgm.resources.Room;
@@ -14,27 +21,35 @@ public class DolphinWriter {
 	String FileFolder;
 	File location;
 
-	public DolphinWriter(DolphinFrame df, GmFile gmFile, File file)
-	{
+	public DolphinWriter(DolphinFrame df, GmFile gmFile, File file) {
 		df.progress(1, "Starting to parse files", "Starting Dolphin");
 		this.df = df;
 		this.gmFile = gmFile;
 		this.location = file;
-		this.FileFolder = file.getPath()+File.separator+"Dolphin Projects"+File.separator+gmFile.filename+File.separator;
-	new File(FileFolder).mkdirs();
+		this.FileFolder = file.getPath() + File.separator + "Dolphin Projects"
+				+ File.separator + gmFile.filename + File.separator;
+		new File(FileFolder).mkdirs();
 	}
+
 	
-	void parseRooms()
-	{
-		//out.write4(f.rooms.size());
-		for (Room r : gmFile.rooms)
-			{
+
+	public void createFolders() {
+		System.out.println("Moving folders");
+
+	}
+
+	void parseRooms() {
+		// out.write4(f.rooms.size());
+		for (Room r : gmFile.rooms) {
 			try {
-			FileWriter sceneFW = new FileWriter(FileFolder + r.getName() + ".java");
-	        BufferedWriter scene = new BufferedWriter(sceneFW);
-	        
-			}catch(Exception e){e.printStackTrace();}
+				FileWriter sceneFW = new FileWriter(FileFolder + r.getName()
+						+ ".java");
+				BufferedWriter scene = new BufferedWriter(sceneFW);
+
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+		}
 	}
-	
+
 }
--- Dolphin/src/org/lgj/Plugin.java
+++ Dolphin/src/org/lgj/Plugin.java
@@ -2,6 +2,16 @@ package org.lgj;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -17,9 +27,10 @@ public class Plugin implements ActionListener, SubframeListener {
 	public JMenuItem run, debug, build, compile;
 	public DolphinFrame df;
 	public DolphinWriter dw;
+	public File pluginLocation;
 	
 	public Plugin() {
-		JMenu menu = new JMenu("LGJava");
+		JMenu menu = new JMenu("Dolphin");
 
 		run = new JMenuItem("Run");
 		run.addActionListener(this);
@@ -36,6 +47,20 @@ public class Plugin implements ActionListener, SubframeListener {
 
 		LGM.frame.getJMenuBar().add(menu,5);
 		SubframeInformer.addSubframeListener(this);
+		
+		/*
+		 * Get the plugin location
+		 */
+		try
+		{
+		pluginLocation = new File(Plugin.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+		}
+	catch (URISyntaxException e1)
+		{
+		e1.printStackTrace();
+		}
+		
+		extractRunners();
 	}
 
 	@Override
@@ -100,6 +125,61 @@ public class Plugin implements ActionListener, SubframeListener {
 	public boolean subframeRequested(Resource<?> res, ResNode node) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	/*
+	 * 
+	 * Copy an InputStream to an OutputStream
+	 * 
+	 */
+	public static final void copyInputStream(InputStream in, OutputStream out)
+			throws IOException {
+		byte buffer[] = new byte[1024];
+		int len;
+		while ((len = in.read(buffer)) >= 0) {
+			out.write(buffer, 0, len);
+		}
+		in.close();
+		out.close();
+	}
+
+	/*
+	 * Extract the java class files from the plugin to the plugins folder of lgm so they can be copied later on
+	 * to the directory of the game project.
+	 */
+	public void extractRunners() {
+		System.out.println("plugin dir:"+this.pluginLocation);
+		try {
+			ZipFile zipFile = new ZipFile(this.pluginLocation);
+
+			for (Enumeration entries = zipFile.entries(); entries
+					.hasMoreElements();) {
+				ZipEntry entry = (ZipEntry) entries.nextElement();
+				if (entry.isDirectory()) {
+					System.out.println("Folder:" + entry.getName());
+					(new File(System.getProperty("user.dir")+File.separator+"plugins" + File.separator
+							+ (entry.getName()).toString())).mkdirs();
+				} else {
+					new File(System.getProperty("user.dir")+File.separator+
+											"plugins" + File.separator).mkdirs();
+					copyInputStream(zipFile.getInputStream(entry),
+							new BufferedOutputStream(new FileOutputStream(System.getProperty("user.dir")+File.separator+
+											"plugins" + File.separator+
+											entry.getName())));
+				}
+			}
+
+			zipFile.close();
+
+		} catch (IOException ioe) {
+
+			ioe.printStackTrace();
+			// JOptionPane.showMessageDialog(null, "Error unziping, are you sure
+			// it was a valid zip file? The link has to be directly to a zip not
+			// to a php page!");
+
+			// return;
+		}
 	}
 	
 }
